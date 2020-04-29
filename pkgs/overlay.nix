703ca23daefb4dc6f0c35d68ff4f0125b148636a
Maciej Krüger <mkg20001@gmail.com>
2020-04-29 16:42:02 +0200

    feat: put everything into it's own folder

--- pkgs/overlay.nix
+++ pkgs/overlay.nix
@@ -1,11 +1,11 @@
 self: super: {
-  openvr = super.callPackage ./openvr.nix {};
-  openvrSamples = super.callPackage ./openvr-samples.nix {};
+  openvr = super.callPackage ./openvr {};
+  openvrSamples = super.callPackage ./openvr-samples {};
 
-  gulkan = super.callPackage ./gulkan.nix {};
-  gxr = super.callPackage ./gxr.nix {};
-  libinputsynth = super.callPackage ./libinputsynth.nix {};
-  xrdesktop = super.callPackage ./xrdesktop.nix {};
+  gulkan = super.callPackage ./gulkan {};
+  gxr = super.callPackage ./gxr {};
+  libinputsynth = super.callPackage ./libinputsynth {};
+  xrdesktop = super.callPackage ./xrdesktop {};
 
   plasma5 = super.plasma5.overrideScope' (plasma-self: plasma-super: rec {
     # Ensure the kwin-effect-xrdesktop plugin runnin in kwin has access to xrdesktop gsettings-schemas
@@ -13,8 +13,8 @@ self: super: {
       qtWrapperArgs = [ ''--prefix XDG_DATA_DIRS  : ${self.xrdesktop}/share/gsettings-schemas/${self.xrdesktop.name}'' ];
     });
 
-    kwin-effect-xrdesktop = super.libsForQt5.callPackage ./kwin-effect-xrdesktop.nix {};
-    kdeplasma-applets-xrdesktop = super.libsForQt5.callPackage ./kdeplasma-applets-xrdesktop.nix { inherit kwin-effect-xrdesktop; }; # Probably not the right way to do this.
+    kwin-effect-xrdesktop = super.libsForQt5.callPackage ./kwin-effect-xrdesktop {};
+    kdeplasma-applets-xrdesktop = super.libsForQt5.callPackage ./kdeplasma-applets-xrdesktop { inherit kwin-effect-xrdesktop; }; # Probably not the right way to do this.
   });
 
   gnome3 = super.gnome3.overrideScope' (gnome-self: gnome-super: {
@@ -37,6 +37,6 @@ self: super: {
   });
 
   gnomeExtensions = super.gnomeExtensions // {
-    xrdesktop = super.callPackage ./gnome-shell-extension-xrdesktop.nix {};
+    xrdesktop = super.callPackage ./gnome-shell-extension-xrdesktop {};
   };
 }

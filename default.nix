f68c5c05333eb64f31bbcdc9d2f561fa3c5b2b1e
Maciej Krüger <mkg20001@gmail.com>
2020-04-29 16:36:07 +0200

    feat: restructure

--- default.nix
+++ default.nix
@@ -2,13 +2,9 @@
 
 with lib;
 {
-  nixpkgs.overlays = [ (import ./overlay.nix) ];
+  nixpkgs.overlays = [
+    (import ./overlay.nix)
+  ];
 
-  environment.systemPackages = with pkgs;
-    let
-      desktopManager = config.services.xserver.desktopManager;
-    in
-      optionals desktopManager.plasma5.enable [ plasma5.kwin-effect-xrdesktop plasma5.kdeplasma-applets-xrdesktop ];
-
-  services.xserver.desktopManager.gnome3.sessionPath = [ pkgs.gnomeExtensions.xrdesktop ];
+  services.xrdesktop.enable = true;
 }
---
+++ modules/xrdesktop.nix
@@ -0,0 +1,25 @@
+{ config, pkgs, lib, ... }:
+
+with lib;
+
+let
+  desktopManager = config.services.xserver.desktopManager;
+in
+{
+  options.services.xrdesktop.enable = mkEnableOption "xrdesktop";
+
+  config = mkIf config.xrdesktop.enable
+    (optionalAttrs desktopManager.plasma5.enable {
+      environment.systemPackages = with pkgs; [ plasma5.kwin-effect-xrdesktop plasma5.kdeplasma-applets-xrdesktop ];
+      })
+    //
+    (optionalAttrs desktopManager.gnome3.enable {
+      services.xserver.desktopManager.gnome3.sessionPath = with pkgs; [ gnomeExtensions.xrdesktop ];
+      })
+    //
+    {
+      # Ensure SteamVR is working correctly:
+      hardware.steam-hardware.enable = true;
+      environment.systemPackages = [ steam ];
+    };
+}
